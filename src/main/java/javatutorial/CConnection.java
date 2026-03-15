2550c280a6e83e739ef8513ee33b0c82120b3354
Raddin Pratama Rachmat <142906084+Capsradd@users.noreply.github.com>
2026-03-15 13:24:19 +0700

    Update CConnection.java
    nambahin langsung buat username sama passwordnya, karna di readme.md ada di bagiann konfigurasi koneksi database, tapi di kodenya harus input sendiri (syntaxnya)

--- src/main/java/javatutorial/CConnection.java
+++ src/main/java/javatutorial/CConnection.java
@@ -5,14 +5,16 @@ import java.sql.*;
 public class CConnection {
     public Connection cn;
 
+    private String url = "jdbc:mysql://localhost:3306/karyawan_db"; // kalo ngikutin tutorial ga usah di ubah
+    private String user = "karyawan_user";    // Sesuaikan sama username MySQL Anda
+    private String password = "password123"; // Sesuaikan sama password MySQL Anda
+
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DATABASE_URL = "jdbc:mysql://localhost/latihan?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
    public void openConnection() {
     try {
         Class.forName(JDBC_DRIVER);
-        // GANTI JADI INI:
-        cn = DriverManager.getConnection(DATABASE_URL, "root", "");
+        cn = DriverManager.getConnection(url, user, password);
         System.out.println("✅ Database Connected!");
     } catch (Exception ex) {
         ex.printStackTrace();
